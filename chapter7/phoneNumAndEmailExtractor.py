c1628dca2778b013ff0a6784a2849d047de3adb6
Renee Marsland <reneemarsland@hotmail.com>
2018-07-04 17:57:47 +0930

    Update phoneNumAndEmailExtractor.py

--- chapter7/phoneNumAndEmailExtractor.py
+++ chapter7/phoneNumAndEmailExtractor.py
@@ -5,20 +5,20 @@ import pyperclip, re
 
 # Regex to search for phone numbers.
 phoneRegex = re.compile(r'''(
-	(\d{3}|\(\d{3}\))?					# Area code.		Optional area code, so the area code group is followed by a quesiton mark. It can be 3 digits OR 3 digits in a parenthesis.
-	(\s|-|\.)?						# Seperator.		A seperator can be a space, hyphen, or period.
-	(\d{3})							# First 3 digits.	
-	(\s|-|\.)						# Seperator.
-	(\d{4})							# Last 4 digits.
-	(\s*(ext|x|ext.)\s*(\d{2,5}))?				# Extension.		An optional extension made up of spaces followed by ext, x, or ext. Followed by two to five digits.
+	(\d{3}|\(\d{3}\))?				# Area code.		Optional area code, so the area code group is followed by a quesiton mark. It can be 3 digits OR 3 digits in a parenthesis.
+	(\s|-|\.)?					# Seperator.		A seperator can be a space, hyphen, or period.
+	(\d{3})						# First 3 digits.	
+	(\s|-|\.)					# Seperator.
+	(\d{4})						# Last 4 digits.
+	(\s*(ext|x|ext.)\s*(\d{2,5}))?			# Extension.		An optional extension made up of spaces followed by ext, x, or ext. Followed by two to five digits.
 	)''', re.VERBOSE)				
 
 # Regex to search for emails.
 emailRegex = re.compile(r'''(
-	[a-zA-Z0-9._%+-]+				# Username.			One or more characters that can be any of the following: lowercase and uppercase lettersm numbers, a dot, and underscore, a percent sign, a plus sign, or a hyphen.
-	@						# @ symbol.			Username and domain are seperated by a @ symbol.
-	[a-zA-Z0-9.-]+					# Domain name.			One or more characters that can be any of the following: letters, numbers, periods, and hyphens.
-	(\.[a-zA-Z]{2,4})				# Dot-something.		Between 2 and 4 characters long of letters.
+	[a-zA-Z0-9._%+-]+				# Username.		One or more characters that can be any of the following: lowercase and uppercase lettersm numbers, a dot, and underscore, a percent sign, a plus sign, or a hyphen.
+	@						# @ symbol.		Username and domain are seperated by a @ symbol.
+	[a-zA-Z0-9.-]+					# Domain name.		One or more characters that can be any of the following: letters, numbers, periods, and hyphens.
+	(\.[a-zA-Z]{2,4})				# Dot-something.	Between 2 and 4 characters long of letters.
 	)''', re.VERBOSE)
 
 # Find the matches in the clipboard text.
